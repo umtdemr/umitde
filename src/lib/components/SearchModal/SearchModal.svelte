125cfc7fc314803d979e784ab828399254916eaa
umtdemr <umitde692@gmail.com>
2024-03-21 19:59:54 +0300

    add Search component

---
+++ src/lib/components/SearchModal/SearchModal.svelte
@@ -0,0 +1,9 @@
+<div class="backdrop">
+    <div
+        role="alertdialog"
+        aria-modal="true"
+        aria-label="Search"
+        aria-describedby="Search in Ümit Demir's blog posts">
+        
+    </div>
+</div>
