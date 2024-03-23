cf38a95bcaa925c5238d833d7f0de336e965d049
umtdemr <umitde692@gmail.com>
2024-03-23 21:55:49 +0300

    design portfolio items

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -5,4 +5,67 @@
 <section class="portfolio">
     <h2 class="section_title">Portfolio</h2>
     <p class="section_desc">Lorem ipsum dolar sit amöet</p>
+    <div class="portfolio_list">
+        <div class="portfolio_item">
+            <div class="portfolio_item__img" ></div> <!-- TODO: REPLACE WITH IMAGE TAG -->
+            <h3 class="portfolio_item__title">Mimic Robot</h3>
+            <div class="portfolio_tags">
+                <div class="tag_item purple">robotics</div>
+            </div>
+        </div>
+        <div class="portfolio_item">
+            <div class="portfolio_item__img" ></div> <!-- TODO: REPLACE WITH IMAGE TAG -->
+            <h3 class="portfolio_item__title">Mimic Robot</h3>
+            <div class="portfolio_tags">
+                <div class="tag_item purple">robotics</div>
+            </div>
+        </div>
+    </div>
 </section>
+
+
+<style lang="scss">
+    .portfolio_list {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 365px 365px;
+      gap: 28px;
+      margin-top: 65px;
+      user-select: none;
+    }
+    .portfolio_item {
+      height: 405px;
+      border-radius: 33px;
+      position: relative;
+      padding: 30px 15px;
+      &__img {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: var(--color-modal-dark-bg);
+        border-radius: 33px;
+      }
+      &__title {
+        color: var(--color-text-1);
+        font-size: 1.5rem;
+        position: relative;
+      }
+    }
+    .portfolio_tags {
+      display: flex;
+      gap: 10px;
+      position: relative;
+      margin-top: 10px;
+      .tag_item {
+        padding: 5px 10px;
+        border-radius: 20px;
+        font-size: 0.8rem;
+        color: var(--color-text-1);
+        &.purple {
+          background-color: #4F55ED;
+        }
+      }
+    }
+</style>
